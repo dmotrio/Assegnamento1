b54390e27200474ca2b840ed271aee877d172846
OllariDmitri <dimoolla200@gmail.com>
2021-10-16 00:34:21 +0200

    aggiunto shoppingcart e orders class

--- src/it/unipr/sowide/OllariIschimjiDmitri/Main.java
+++ src/it/unipr/sowide/OllariIschimjiDmitri/Main.java
@@ -1,13 +1,17 @@
 package it.unipr.sowide.OllariIschimjiDmitri;
 
 import it.unipr.sowide.OllariIschimjiDmitri.Store.OnlineShop;
+import it.unipr.sowide.OllariIschimjiDmitri.Store.Orders;
 import it.unipr.sowide.OllariIschimjiDmitri.Store.Product;
+import it.unipr.sowide.OllariIschimjiDmitri.Store.ShoppingCart;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         OnlineShop onlineShop = new OnlineShop();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Orders orders = new Orders();
 
         Product a = new Product("iphone1", "apple", 123.4f, 100);
         Product b = new Product("iphone2", "apple", 123.4f, 10);
@@ -47,5 +51,14 @@ public class Main {
             System.out.println(onlineShop.getQuantityOfProducts().get(i));
         }
 
+        shoppingCart.addProductToShoppingCart(a, 100);
+        shoppingCart.addProductToShoppingCart(c, 10);
+
+        System.out.println();
+        System.out.println();
+        System.out.println("ShoppingList");
+        for (int i = 0; i < shoppingCart.getShoppingCart().size(); i++){
+            System.out.println(shoppingCart.getShoppingCart().get(i).toString());
+        }
     }
 }
